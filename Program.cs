91358725fe9fbbfc93d31b2902c59ff495f68f7c
«Alex18» <alexburtsev18@yandex.ru>
2023-04-15 20:31:11 +0300

    Сделал сам вторую задачу

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Task 1. Напишите программу, которая на вход принимает случайное трехзначное число
+// и удаляет вторую цифру этого числа.
+
+// int CutNumber(int num)
+// {
+//     int hundreds = num / 100;
+//     int unites = num % 10;
+//     int result = hundreds * 10 + unites;
+//     return result;
+// }
+
+// int randNumber = new Random().Next(100, 1000);
+// int shortNumber = CutNumber(randNumber);
+// Console.WriteLine($"New version of {randNumber} is {shortNumber}");
+
+// Task 2++. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// int Numeral(int num)
+// {
+//     int decade = num / 10;
+//     return decade;
+// }
+// int randNumber = new Random().Next(10, 100);
+// int firstNum = Numeral(randNumber);
+// Console.WriteLine($"First number of {randNumber} is {firstNum}");
+
+
+// Task 3. Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому.
+
+
+// Task 4. Напишите программу, которая принимает на вход число N и проверяет, кратно ли оно одновременно a и b.
+
+
+// task 5. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом второго.
